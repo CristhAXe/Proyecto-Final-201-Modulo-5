63e3cf0b7a424af8e270e9ffcb854bc3c4642dba
Cristh Nunonk <znkcristh@protonmail.com>
2026-05-08 19:00:13 -0500

    se-bienvenida-tailwing.js

---
+++ assets/tailwind/bienvenida-tailwind.js
@@ -0,0 +1,79 @@
+        tailwind.config = {
+            darkMode: "class",
+            theme: {
+                extend: {
+                    "colors": {
+                        "outline-variant": "#c3c6d5",
+                        "primary-fixed-dim": "#afc6ff",
+                        "on-primary": "#ffffff",
+                        "error-container": "#ffdad6",
+                        "primary-fixed": "#d9e2ff",
+                        "inverse-on-surface": "#f0f0f9",
+                        "primary-container": "#0050b5",
+                        "tertiary-fixed": "#ffdbcd",
+                        "surface-container-high": "#e7e7f1",
+                        "on-background": "#191b22",
+                        "on-primary-fixed": "#001944",
+                        "on-error": "#ffffff",
+                        "background": "#faf8ff",
+                        "on-tertiary-fixed": "#360f00",
+                        "surface-container": "#ededf6",
+                        "secondary-fixed": "#36ffc4",
+                        "on-surface": "#191b22",
+                        "on-tertiary-container": "#ffba9e",
+                        "tertiary": "#6f2600",
+                        "surface-variant": "#e1e2eb",
+                        "secondary-container": "#00fabe",
+                        "on-tertiary": "#ffffff",
+                        "surface-dim": "#d9d9e2",
+                        "on-secondary-fixed-variant": "#00513c",
+                        "surface-container-low": "#f3f3fc",
+                        "on-secondary-fixed": "#002116",
+                        "on-primary-container": "#b5caff",
+                        "inverse-surface": "#2e3037",
+                        "secondary-fixed-dim": "#00e1ab",
+                        "tertiary-container": "#953600",
+                        "on-primary-fixed-variant": "#004299",
+                        "on-secondary": "#ffffff",
+                        "on-secondary-container": "#006f52",
+                        "surface-container-lowest": "#ffffff",
+                        "secondary": "#006c50",
+                        "tertiary-fixed-dim": "#ffb597",
+                        "inverse-primary": "#afc6ff",
+                        "error": "#ba1a1a",
+                        "on-tertiary-fixed-variant": "#7e2c00",
+                        "surface-tint": "#195abf",
+                        "on-surface-variant": "#424753",
+                        "surface-container-highest": "#e1e2eb",
+                        "primary": "#003a87",
+                        "on-error-container": "#93000a",
+                        "surface": "#faf8ff",
+                        "outline": "#737784",
+                        "surface-bright": "#faf8ff"
+                    },
+                    "borderRadius": {
+                        "DEFAULT": "0.25rem",
+                        "lg": "0.5rem",
+                        "xl": "0.75rem",
+                        "full": "9999px"
+                    },
+                    "spacing": {
+                        "stack-sm": "4px",
+                        "stack-lg": "24px",
+                        "container-padding": "24px",
+                        "base": "8px",
+                        "stack-md": "12px",
+                        "gutter": "16px"
+                    },
+                    "fontFamily": {
+                        "body-lg": ["Plus Jakarta Sans"],
+                        "label-caps": ["Plus Jakarta Sans"],
+                        "display": ["Plus Jakarta Sans"],
+                        "headline-md": ["Plus Jakarta Sans"],
+                        "headline-lg": ["Plus Jakarta Sans"],
+                        "body-sm": ["Plus Jakarta Sans"]
+                    }
+                }
+            }
+        }
+    
